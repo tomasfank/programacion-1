efa627452231299ab49d4f04e70066de0bf3f16f
tomasfank <tomasfank@outlook.com>
2023-04-14 16:17:08 -0300

    ejercicio2

--- TP_2/ejercicio2.py
+++ TP_2/ejercicio2.py
@@ -10,3 +10,42 @@
     
 Combinar estas tres funciones en un mismo programa """
 
+import random 
+
+def numerosAzar(x):
+    lista = []
+    for i in range(x):
+        lista.append(random.randint(1, 100))
+    return lista
+
+def repetidos(lista):
+    r = False
+    for x in range(len(lista)):
+        if lista.count(lista[x]) > 1:
+            r = True
+    return r
+
+def sinRepetidos(lista):
+    listaNueva = []
+    for i in range(len(lista)):
+        if lista[i] not in listaNueva:
+            listaNueva.append(lista[i])
+    return listaNueva
+
+#programa principal
+a = numerosAzar(50)
+print(a)
+b = repetidos(a)
+if b == True:
+    print("La lista contiene elementos repetidos")
+    c = sinRepetidos(a)
+    print(c)
+    #para facilitar el análisis imprimo las listas nuevamente, pero de manera ordenada
+    print()
+    a.sort()
+    c.sort()
+    print(a) 
+    print(c)
+else:
+    print("La lista no contiene elementos repetidos")
+
